9614185e52bd979e7ee8a174345db7e810195f2c
yaorui <anqingyaorui@vip.qq.com>
2015-08-17 10:20:25 +0800

    new

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -293,7 +293,7 @@ Draw.prototype.drawSame = function (context) {
 				color : ['#ffa634','#fee147','#c4df42','#eeb623'],
 			    title : {
 			        text: '新生中和你同年同月的比例为'+Math.round(data.same.samemon/(data.same.samemon+data.same.others)*10000)/100.000+'%',
-			        subtext: '新生中和你相同星座的比例为 ' + Math.round(data.same.samehor/(data.same.samehor+data.same.othershor)*10000)/100.000+'%',
+			        subtext: '新生中和你相同星座的比例为 ' + Math.round(data.same.samehor/(data.same.samehor+data.same.othershor)*10000)/100.000+'%<br/>和你同年同月同日的男生有'+samemale+'人，女生有'+samefemale+'人',
 			        x: 'center',
 			        textStyle: {
 			        	color: '#d87e1d',
